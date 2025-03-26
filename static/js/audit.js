ff50f19bb4bcd32eec0a3c363b0a61a16c1457aa
dni ⚡ <office@dnilabs.com>
2025-03-26 14:09:43 +0100

    feat: `.prettierrc` config from lnbits (#20)

--- static/js/audit.js
+++ static/js/audit.js
@@ -1,5 +1,5 @@
 window.app = Vue.createApp({
-  el: "#vue",
+  el: '#vue',
   mixins: [window.windowMixin],
   data: function () {
     return {
@@ -7,72 +7,72 @@ window.app = Vue.createApp({
       auditTable: {
         columns: [
           {
-            name: "id",
-            align: "left",
-            label: "ID",
-            field: "id",
-            sortable: true,
+            name: 'id',
+            align: 'left',
+            label: 'ID',
+            field: 'id',
+            sortable: true
           },
           {
-            name: "entry_id",
-            align: "left",
-            label: "Entry ID",
-            field: "entry_id",
-            sortable: true,
+            name: 'entry_id',
+            align: 'left',
+            label: 'Entry ID',
+            field: 'entry_id',
+            sortable: true
           },
           {
-            name: "created_at",
-            align: "left",
-            label: "Created At",
-            field: "created_at",
-            format: (val) => LNbits.utils.formatDateString(val),
-            sortable: true,
+            name: 'created_at',
+            align: 'left',
+            label: 'Created At',
+            field: 'created_at',
+            format: val => LNbits.utils.formatDateString(val),
+            sortable: true
           },
           {
-            name: "data",
-            align: "left",
-            label: "Data",
-            field: "data",
-            sortable: false,
-          },
+            name: 'data',
+            align: 'left',
+            label: 'Data',
+            field: 'data',
+            sortable: false
+          }
         ],
         pagination: {
           rowsPerPage: 10,
           page: 1,
-          rowsNumber: 10,
+          rowsNumber: 10
         },
-        search: "",
+        search: ''
       },
       auditForm: {
         show: false,
-        entryId: entry_id,
-      },
-    };
+        entryId: entry_id
+      }
+    }
   },
   watch: {
-    "auditTable.search": {
+    'auditTable.search': {
       handler() {
-        this.getAuditEntriesPaginated();
-      },
-    },
+        this.getAuditEntriesPaginated()
+      }
+    }
   },
   methods: {
     getAuditEntriesPaginated: async function (props) {
       try {
-        const params = LNbits.utils.prepareFilterQuery(this.auditTable, props);
-        const { data } = await LNbits.api.request(
-          "GET",
+        const params = LNbits.utils.prepareFilterQuery(this.auditTable, props)
+        const {data} = await LNbits.api.request(
+          'GET',
           `/auction_house/api/v1/audit/items` +
-            `/${this.auditForm.entryId}/paginated?${params}`,
-        );
-        this.auditEntries = data.data;
-        this.auditTable.pagination.rowsNumber = data.total;
+            `/${this.auditForm.entryId}/paginated?${params}`
+        )
+        this.auditEntries = data.data
+        this.auditTable.pagination.rowsNumber = data.total
       } catch (error) {
-        LNbits.utils.notifyApiError(error);
+        LNbits.utils.notifyApiError(error)
       }
-    },
+    }
   },
   created() {
-    this.getAuditEntriesPaginated();
-  },
-});
+    this.getAuditEntriesPaginated()
+  }
+})
--- static/js/index.js
+++ static/js/index.js
@@ -1,166 +1,166 @@
 window.app = Vue.createApp({
-  el: "#vue",
+  el: '#vue',
   mixins: [window.windowMixin],
   data: function () {
     return {
       auctionRooms: [],
       biddingType: [
-        { value: "auction", label: "Auction" },
-        { value: "fixed_price", label: "Fixed Price" },
+        {value: 'auction', label: 'Auction'},
+        {value: 'fixed_price', label: 'Fixed Price'}
       ],
 
       currencyOptions: [],
       auctionRoomsTable: {
         columns: [
           {
-            name: "created_at",
-            align: "left",
-            label: "Created At",
-            field: "created_at",
-            format: (val) => LNbits.utils.formatDateString(val),
+            name: 'created_at',
+            align: 'left',
+            label: 'Created At',
+            field: 'created_at',
+            format: val => LNbits.utils.formatDateString(val)
           },
 
           {
-            name: "name",
-            align: "left",
-            label: "Name",
-            field: "name",
+            name: 'name',
+            align: 'left',
+            label: 'Name',
+            field: 'name'
           },
           {
-            name: "currency",
-            align: "left",
-            label: "Currency",
-            field: "currency",
+            name: 'currency',
+            align: 'left',
+            label: 'Currency',
+            field: 'currency'
           },
           {
-            name: "type",
-            align: "left",
-            label: "Type",
-            field: "type",
-            format: (val) => (val === "auction" ? "🔨" : "💰"),
+            name: 'type',
+            align: 'left',
+            label: 'Type',
+            field: 'type',
+            format: val => (val === 'auction' ? '🔨' : '💰')
           },
-          { name: "days", align: "left", label: "Days", field: "days" },
+          {name: 'days', align: 'left', label: 'Days', field: 'days'},
           {
-            name: "room_percentage",
-            align: "left",
-            label: "Room %",
-            field: "room_percentage",
+            name: 'room_percentage',
+            align: 'left',
+            label: 'Room %',
+            field: 'room_percentage'
           },
           {
-            name: "min_bid_up_percentage",
-            align: "left",
-            label: "Min Bid %",
-            field: "min_bid_up_percentage",
+            name: 'min_bid_up_percentage',
+            align: 'left',
+            label: 'Min Bid %',
+            field: 'min_bid_up_percentage'
           },
-          { name: "id", align: "left", label: "Room Id", field: "id" },
+          {name: 'id', align: 'left', label: 'Room Id', field: 'id'}
         ],
         pagination: {
-          rowsPerPage: 10,
-        },
+          rowsPerPage: 10
+        }
       },
 
       formDialog: {
         show: false,
-        data: {},
-      },
-    };
+        data: {}
+      }
+    }
   },
   methods: {
     resetFormDialog: function () {
-      this.formDialog.show = false;
-      this.auctionRoomTab = "webhooks";
+      this.formDialog.show = false
+      this.auctionRoomTab = 'webhooks'
       this.formDialog.data = {
-        name: "",
-        description: "",
-        wallet_id: "",
-        currency: "sat",
-        type: null,
-      };
+        name: '',
+        description: '',
+        wallet_id: '',
+        currency: 'sat',
+        type: null
+      }
     },
     showFormDialog: function () {
-      this.resetFormDialog();
-      this.formDialog.show = true;
+      this.resetFormDialog()
+      this.formDialog.show = true
     },
 
     getAuctionRooms: async function () {
       try {
-        const { data } = await LNbits.api.request(
-          "GET",
-          "/auction_house/api/v1/auction_room",
-        );
+        const {data} = await LNbits.api.request(
+          'GET',
+          '/auction_house/api/v1/auction_room'
+        )
 
-        this.auctionRooms = data;
+        this.auctionRooms = data
       } catch (error) {
-        LNbits.utils.notifyApiError(error);
+        LNbits.utils.notifyApiError(error)
       }
     },
 
     saveAuctionRoom: async function () {
       try {
-        const { data } = await LNbits.api.request(
-          "POST",
-          "/auction_house/api/v1/auction_room",
+        const {data} = await LNbits.api.request(
+          'POST',
+          '/auction_house/api/v1/auction_room',
           null,
-          this.formDialog.data,
-        );
+          this.formDialog.data
+        )
 
-        this.auctionRooms.push(data);
-        this.resetFormDialog();
+        this.auctionRooms.push(data)
+        this.resetFormDialog()
       } catch (error) {
-        LNbits.utils.notifyApiError(error);
+        LNbits.utils.notifyApiError(error)
       }
     },
 
     deleteAuctionRoom: function (auction_room_id) {
-      var self = this;
+      var self = this
       var auction_room = _.findWhere(this.auctionRooms, {
-        id: auction_room_id,
-      });
+        id: auction_room_id
+      })
 
       LNbits.utils
-        .confirmDialog("Are you sure you want to delete this auction room?")
+        .confirmDialog('Are you sure you want to delete this auction room?')
         .onOk(function () {
           LNbits.api
             .request(
-              "DELETE",
-              "/auction_house/api/v1/auction_room/" + auction_room_id,
-              _.findWhere(self.g.user.wallets, { id: auction_room.wallet_id })
-                .adminkey,
+              'DELETE',
+              '/auction_house/api/v1/auction_room/' + auction_room_id,
+              _.findWhere(self.g.user.wallets, {id: auction_room.wallet_id})
+                .adminkey
             )
             .then(function (response) {
               self.auctionRooms = self.auctionRooms.filter(
-                (d) => d.id !== auction_room_id,
-              );
+                d => d.id !== auction_room_id
+              )
             })
             .catch(function (error) {
-              LNbits.utils.notifyApiError(error);
-            });
-        });
+              LNbits.utils.notifyApiError(error)
+            })
+        })
     },
     exportCSV: function () {
-      LNbits.utils.exportCSV(this.auctionRoomsTable.columns, this.auctionRooms);
-    },
+      LNbits.utils.exportCSV(this.auctionRoomsTable.columns, this.auctionRooms)
+    }
   },
   created() {
-    this.resetFormDialog();
+    this.resetFormDialog()
 
-    this.getAuctionRooms();
+    this.getAuctionRooms()
 
     LNbits.api
-      .request("GET", "/api/v1/currencies")
-      .then((response) => {
-        this.currencyOptions = ["sat", ...response.data];
+      .request('GET', '/api/v1/currencies')
+      .then(response => {
+        this.currencyOptions = ['sat', ...response.data]
       })
-      .catch(LNbits.utils.notifyApiError);
+      .catch(LNbits.utils.notifyApiError)
   },
   computed: {
     auctionRoomOptions: function () {
-      return this.auctionRooms.map((el) => {
+      return this.auctionRooms.map(el => {
         return {
           label: el.auction_room,
-          value: el.id,
-        };
-      });
-    },
-  },
-});
+          value: el.id
+        }
+      })
+    }
+  }
+})
